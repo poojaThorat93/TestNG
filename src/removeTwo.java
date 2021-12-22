c9cace4dfe1bde3f16ae0673f1292629e641a1cf
poojaThorat93 <95961879+poojaThorat93@users.noreply.github.com>
2021-12-22 10:49:05 +0530

    Update removeTwo.java

--- src/removeTwo.java
+++ src/removeTwo.java
@@ -1,8 +1,7 @@
 
 public class removeTwo{
-	
-	 void Login() {
-	System.out.println(" This is login ");
+		 void Login() {
+	System.out.println(" This is login ++++ ");
      System.out.println(" This is Registration ");
 		 
 	}
